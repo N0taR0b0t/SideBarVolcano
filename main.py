e0c83b3bcf0651693a24c11d65d3b4fe3c77e96c
N0tar0b0t <mxbadino@gmail.com>
2025-06-18 13:22:21 -0400

    Requirements.txt update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from column_mapper import extract_column_mapping, save_mapping
 from distCalc import compute_compound_distances
 
 # ─────────────────────────────────────────────────────────────
-IDENTIFIER = "re"
+IDENTIFIER = "plasma"
 REQUIRED_COLUMN = "Compounds ID"
 ENCODING = "latin1"
 DELIMITER = ","
