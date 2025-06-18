15320bf394f39287e020ff08f62765467d7dc110
N0tar0b0t <mxbadino@gmail.com>
2025-06-18 14:03:03 -0400

    Re-main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from column_mapper import extract_column_mapping, save_mapping
 from distCalc import compute_compound_distances
 
 # ─────────────────────────────────────────────────────────────
-IDENTIFIER = "plasma"
+IDENTIFIER = "Re"
 REQUIRED_COLUMN = "Compounds ID"
 ENCODING = "latin1"
 DELIMITER = ","
